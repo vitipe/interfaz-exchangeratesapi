75b11b998a68d43235ab206276d2072a20322bcb
vitipe <victorcasbas@gmail.com>
2021-03-11 18:20:27 -0300

    agrega function que hace el fetch al latest

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 
+function muestraCurrenciesDelDia() {
+    fetch('https://api.exchangeratesapi.io/latest')
+    .then(respuesta => respuesta.json())
+    .then(data => console.log(data))  // despues del arrow le digo que es lo que quiero hacer(crear los elementos de la tabla)
+}
+
 function mostrarDiaActual() {
     let hoy = new Date().toLocaleDateString("es-AR")
     document.querySelector('#fecha-actual').textContent = hoy;
 }
 
 mostrarDiaActual();
+
+muestraCurrenciesDelDia();
